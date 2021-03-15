56bb32be13cc7eb07bc17289ba7d78c2d1e9924e
kanition <68691995+kanition@users.noreply.github.com>
2021-03-15 21:20:33 +0800

    版本号、边距

--- cp.cmd
+++ cp.cmd
@@ -1,3 +1,7 @@
+echo branch: > ./ver_info.txt
+git rev-parse --abbrev-ref HEAD >> ./ver_info.txt
+echo \quad commit: >> ./ver_info.txt
+git rev-parse HEAD >> ./ver_info.txt
 xelatex -synctex=1 -interaction=nonstopmode -file-line-error main
 makeindex main.idx -s StyleInd.ist
 biber main

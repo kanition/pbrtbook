e61b439211ed94f386376e3ed238cdafa5ff2705
kanition <68691995+kanition@users.noreply.github.com>
2021-04-23 19:03:14 +0800

    编译命令更新

--- cp.cmd
+++ cp.cmd
@@ -6,4 +6,6 @@ xelatex -synctex=1 -interaction=nonstopmode -file-line-error main
 makeindex main.idx -s StyleInd.ist
 biber main
 xelatex -synctex=1 -interaction=nonstopmode -file-line-error main
+makeindex main.idx -s StyleInd.ist
+biber main
 xelatex -synctex=1 -interaction=nonstopmode -file-line-error main

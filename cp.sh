8836ec42f1d391cba849e4ef8bb60f62932a983f
kanition <68691995+kanition@users.noreply.github.com>
2021-11-05 15:24:08 +0800

    4.3修正、编译修正

--- cp.sh
+++ cp.sh
@@ -1,5 +1,3 @@
-sed -i -e 's/PSTricks/pstricks/' structure.tex
-
 echo branch: > ./ver_info.txt
 git rev-parse --abbrev-ref HEAD >> ./ver_info.txt
 echo \quad commit: >> ./ver_info.txt
@@ -11,5 +9,3 @@ xelatex -synctex=1 -interaction=nonstopmode -file-line-error main
 makeindex main.idx -s StyleInd.ist
 biber main
 xelatex -synctex=1 -interaction=nonstopmode -file-line-error main
-
-sed -i -e 's/pstricks/PSTricks/' structure.tex

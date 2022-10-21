2275d5bc7c8913db547a3040bff0db5d36e8d20b
kanition <68691995+kanition@users.noreply.github.com>
2022-10-21 19:54:46 +0800

    fix compile

--- cp.cmd
+++ cp.cmd
@@ -1,12 +1,12 @@
-echo branch: > ./ver_info.txt && ^
-git rev-parse --abbrev-ref HEAD >> ./ver_info.txt && ^
-echo \quad commit: >> ./ver_info.txt && ^
-git rev-parse HEAD >> ./ver_info.txt && ^
-xelatex -synctex=1 -interaction=nonstopmode -file-line-error main && ^
-makeindex main.idx -s StyleInd.ist && ^
-biber main && ^
-xelatex -synctex=1 -interaction=nonstopmode -file-line-error main && ^
-makeindex main.idx -s StyleInd.ist && ^
-biber main && ^
+echo branch: > ./ver_info.txt
+git rev-parse --abbrev-ref HEAD >> ./ver_info.txt
+echo \quad commit: >> ./ver_info.txt
+git rev-parse HEAD >> ./ver_info.txt
+xelatex -synctex=1 -interaction=nonstopmode -file-line-error main
+makeindex main.idx -s StyleInd.ist
+biber main
+xelatex -synctex=1 -interaction=nonstopmode -file-line-error main
+makeindex main.idx -s StyleInd.ist
+biber main
 xelatex -synctex=1 -interaction=nonstopmode -file-line-error main
 @REM gswin64c -sDEVICE=pdfwrite -o "main.pdf" -dCompatibilityLevel=1.4 -dNOPAUSE -dQUIET -dBATCH  "test.pdf"

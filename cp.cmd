4754d82029cd8be833873fbed065c1dab40ec483
Kanition <68691995+kanition@users.noreply.github.com>
2024-02-27 02:10:52 +0800

    set workflow

--- cp.cmd
+++ cp.cmd
@@ -10,3 +10,4 @@ makeindex main.idx -s StyleInd.ist
 biber main
 xelatex -synctex=1 -interaction=nonstopmode -file-line-error main
 @REM gswin64c -sDEVICE=pdfwrite -o "main.pdf" -dCompatibilityLevel=1.4 -dNOPAUSE -dQUIET -dBATCH  "test.pdf"
+echo Finish building

f1bbc29ff7e0f20043f6330b19551d4ff9ed7953
kanition <68691995+kanition@users.noreply.github.com>
2025-07-10 02:15:04 +0800

    8.6 end fix #205 #203

--- cp.sh
+++ cp.sh
@@ -1,6 +1,7 @@
+git config --local --add safe.directory $(pwd)
 echo branch: > ./ver_info.txt
 git rev-parse --abbrev-ref HEAD >> ./ver_info.txt
-echo \quad commit: >> ./ver_info.txt
+echo \\quad commit: >> ./ver_info.txt
 git rev-parse HEAD >> ./ver_info.txt
 xelatex -synctex=1 -interaction=nonstopmode -file-line-error main
 makeindex main.idx -s StyleInd.ist

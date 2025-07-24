f2144ee485a0f06a64618f72753442a0c07ca625
kanition <68691995+kanition@users.noreply.github.com>
2025-07-25 02:23:49 +0800

    8.7 #161 fix #213

--- cp.cmd
+++ cp.cmd
@@ -1,8 +1,10 @@
+git describe --tags --abbrev=0
+git rev-parse HEAD
+
+for /f "delims=" %%i in ('git rev-parse --short HEAD') do set hash_commit=%%i
 echo branch: > ./ver_info.txt
-@REM git describe --tags --abbrev=0
 git rev-parse --abbrev-ref HEAD >> ./ver_info.txt
-echo \quad commit: >> ./ver_info.txt
-git rev-parse HEAD >> ./ver_info.txt
+echo \quad commit: \href{https://github.com/kanition/pbrtbook/commit/%hash_commit%}{%hash_commit%} >> ./ver_info.txt
 xelatex -synctex=1 -interaction=nonstopmode -file-line-error main
 makeindex main.idx -s StyleInd.ist
 biber main
